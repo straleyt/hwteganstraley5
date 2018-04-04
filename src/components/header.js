f9286ccfff6eef65d7caae57eb260d937043027d
straleyt <tegan.straley@ucdenver.edu>
2018-04-04 17:52:16 -0600

    Amy class

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+class MovieHeader extends Component{
+    render() {return <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to web, Tegan</h1>
+        </header>
+        </div>
+        
+    }
+};
---
+++ src/components/movielist.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+class MovieList extends Component{
+
+}
