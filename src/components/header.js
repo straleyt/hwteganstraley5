820d2263ed479bbf6ee19f9512e97e049771815a
straleyt <tegan.straley@ucdenver.edu>
2018-04-04 18:03:22 -0600

    try 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+
+//Header component
 class MovieHeader extends Component{
     render() {return <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to web, Tegan</h1>
         </header>
-        </div>
-        
-    }
-};
+    </div>}};
+
+export default MovieHeader;
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
 
 class MovieList extends Component{
-
+    //state:
 }
